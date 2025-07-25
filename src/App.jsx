8b27d4e1a23b48e1f382a1e788f853cfed98490a
Khun Tu Pi <bwarpay.bp8@gmail.com>
2025-07-25 08:53:51 +0630

    add item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+function Item() {
+  return <li>Item One</li>;
+}
 export default function App() {
-  return <h1>Hello React</h1>
+  return (
+    <>
+      <h1>Hello React</h1>
+      <ul>
+        <Item />
+        <Item />
+        <Item />
+      </ul>
+    </>
+  )
 }
